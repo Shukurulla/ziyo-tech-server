edf3668b6ae37bcabf02cfa0f930615b48ffd467
Shukurulla <shukurullatursinbayev978@gmail.com>
2025-08-11 11:36:42 +0500

    change cors option

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ const app = express();
 app.use(
   cors({
     origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
