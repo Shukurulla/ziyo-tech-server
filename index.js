20bc78571f365a7aa3b889054ab978d1316caa3c
Shukurulla <shukurullatursinbayev978@gmail.com>
2025-08-13 13:16:50 +0500

    start

--- index.js
+++ index.js
@@ -38,11 +38,22 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 const app = express();
 
 // CORS - SODDA VA ISHLAYDIGAN VERSIYA
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+// CORS - eng sodda versiya
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+  res.header("Access-Control-Allow-Credentials", true);
+
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
